06c2f6505aa1c83089faa74647f0b7c272323558
Chris Nyaga <nyagachris75@gmail.com>
2017-08-18 11:48:23 +0300

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817200924) do
+ActiveRecord::Schema.define(version: 20170818084742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170817200924) do
   create_table "order_items", force: :cascade do |t|
     t.integer "product_id"
     t.integer "order_id"
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|
