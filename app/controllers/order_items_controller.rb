6e75d17595e75a616aa10fed61b3ffbaccc3cef9
Chris Nyaga <nyagachris75@gmail.com>
2017-08-18 12:38:55 +0300

    Methods to calculate total removed from the items controller.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,8 +3,6 @@ class OrderItemsController < ApplicationController
     @order = current_order
     @item = @order.order_items.new(item_params)
     @order.save
-    total =  @order.order_items.to_a.inject(0){|sum,x| sum + x.product.price*x.quantity }
-    @order.update(total_price:total)
     session[:order_id] = @order.id
     redirect_to products_path
   end
@@ -14,8 +12,6 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    total =  @order.order_items.to_a.inject(0){|sum,x| sum + x.product.price*x.quantity }
-    @order.update(total_price:total)
     redirect_to cart_path
   end
 
