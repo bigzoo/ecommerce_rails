6a44349add32acaab393471add5e6005b83b85ba
Chris Nyaga <nyagachris75@gmail.com>
2017-08-20 15:09:14 +0300

    Vcr flag added to the tests for the bestseller class.

--- spec/services/get_bestsellers_spec.rb
+++ spec/services/get_bestsellers_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
-
-  describe Bestseller do
+  describe Bestseller, :vcr => true do
     it "returns a 200 success header when the API call is made" do
       response = Bestseller.get_bestseller_list
       expect(response.code).to(eq(200))
